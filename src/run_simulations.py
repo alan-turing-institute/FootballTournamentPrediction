e5fdb5c28f55e9d512ab47357ddd4f4034e34327
nbarlowATI <nbarlow@turing.ac.uk>
2022-10-24 11:41:48 +0100

    use WCPred in run_simulations.py

--- src/run_simulations.py
+++ src/run_simulations.py
@@ -3,10 +3,11 @@ import os
 import pandas as pd
 import argparse
 
-from tournament import Tournament, get_teams_data
+from tournament import Tournament, get_teams_data, get_and_train_model
 
 
 def main(args):
+    model = get_and_train_model()
     teams_df = get_teams_data()
     teams = list(teams_df.Team.values)
     team_results = {
@@ -21,8 +22,8 @@ def main(args):
     }
     for _ in range(args.num_simulations):
         t = Tournament()
-        t.play_group_stage()
-        t.play_knockout_stages()
+        t.play_group_stage(model)
+        t.play_knockout_stages(model)
         for team in teams:
             result = t.get_furthest_position_for_team(team)
             team_results[team][result] += 1
@@ -37,6 +38,3 @@ if __name__ == "__main__":
     parser.add_argument("--output_csv", help="Path to output CSV file", default="sim_results.csv")
     args = parser.parse_args()
     main(args)
-    t = Tournament()
-    t.play_group_stage()
-    t.play_knockout_stages()
