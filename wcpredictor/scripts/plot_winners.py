a4771e626b394a691870ebaa3f8e411580b9a832
jack89roberts <jroberts@turing.ac.uk>
2022-11-16 19:46:02 +0000

    add more options to plotting function

--- wcpredictor/scripts/plot_winners.py
+++ wcpredictor/scripts/plot_winners.py
@@ -5,24 +5,65 @@ import argparse
 import matplotlib.pyplot as plt
 import pandas as pd
 
+round_labels = {
+    "G": "Group stage",
+    "R16": "Round of 16",
+    "QF": "Quarter-final",
+    "SF": "Semi-final",
+    "RU": "Final",
+    "W": "Winner",
+}
+
 
 def main():
     parser = argparse.ArgumentParser("plot the results of WC simulations")
     parser.add_argument("--input_csv", help="location of input file", required=True)
     parser.add_argument("--output_png", help="where to save image")
+    parser.add_argument(
+        "--counts", help="show counts rather than fraction if set", action="store_true"
+    )
+    parser.add_argument(
+        "--num_teams", help="number of teams to plot", type=int, default=10
+    )
+    parser.add_argument(
+        "--round",
+        help="show progression to which round",
+        choices={"G", "R16", "QF", "SF", "RU", "W"},
+        default="W",
+    )
+    parser.add_argument(
+        "--exact_round",
+        help=(
+            "show count of knocked out at round specified rather"
+            "than progression to at least that round if set"
+        ),
+        action="store_true",
+    )
     args = parser.parse_args()
     df = pd.read_csv(args.input_csv)
-    df.sort_values(by="W", axis=0, ascending=False, inplace=True)
-    # plot the top ten
-    df = df[:10]
+    df.set_index("team", inplace=True)
+    if not args.counts:
+        df = 100 * df.div(df.sum(axis=1), axis=0)
+    if not args.exact_round:
+        df = df[["W", "RU", "SF", "QF", "R16", "G"]].cumsum(axis=1)
+    df.sort_values(by=args.round, inplace=True)
+    # plot the top num_teams values
+    df = df[-args.num_teams :]
     fig, ax = plt.subplots(tight_layout=True)
-    xvals = list(df.team.values)
-    yvals = list(df.W.values)
-    ax.bar(xvals, yvals)
-    for label in ax.get_xticklabels():
-        label.set_rotation(45)
-        label.set_ha("right")
-    ax.set_ylabel("Number of wins")
+    xvals = list(df.index)
+    yvals = list(df[args.round].values)
+    ax.barh(xvals, yvals)
+
+    if not args.exact_round:
+        xlabel = "Reached" if args.round != "W" else ""
+    elif args.round == "RU":
+        xlabel = ""
+    else:
+        xlabel = "Knocked out in" if args.round != "W" else ""
+    if args.counts:
+        ax.set_xlabel(f"{xlabel} {round_labels[args.round]} [count]")
+    else:
+        ax.set_xlabel(f"{xlabel} {round_labels[args.round]} [%]")
     if args.output_png:
         plt.savefig(args.output_png)
     else:
